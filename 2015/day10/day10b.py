517dc5767c857ec6e165a251b67bca69a62ed463
pieteromt <{ID}+{username}@users.noreply.github.com>
2024-12-30 13:26:41 +0100

    aoc 2015 day10

---
+++ 2015/day10/day10b.py
@@ -0,0 +1,39 @@
+import sys
+
+from itertools import groupby
+
+def read_data(fname):
+    data = []
+    f = open(fname,"r")
+    for line in f:
+        line = line.strip()
+        data.append(line)
+    f.close()
+    return data
+
+def calc_next(s):
+    a = [''.join(g) for _,g in groupby(s)]
+    result = ""
+    for d in a:
+        result += str(len(d)) + d[0]
+    return result
+        
+def do_loop(s,nr):
+    for i in range(nr):
+        s = calc_next(s)
+    print(len(s))
+
+def process(data):
+    for s in data:
+        do_loop(s,50)
+    
+def day10(fname):
+    data = read_data(fname)
+    process(data)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage: day10 input.txt") 
+    else:
+    	day10(sys.argv[1])
+
