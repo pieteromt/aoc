d587a9b9fafd36c5e50307143182cf604440fa6e
pieteromt <{ID}+{username}@users.noreply.github.com>
2025-02-09 11:14:17 +0100

    aoc 2018 day8

---
+++ 2018/day08/day8b.py
@@ -0,0 +1,58 @@
+import sys
+
+def read_data(fname):
+    data = []
+    f = open(fname,"r")
+    for line in f:
+        line = line.strip()
+        w = line.split()
+        data = list(map(int,w))
+    f.close()
+    return data
+    
+class Node:
+    def __init__(self):
+        self.children = []
+        self.meta = []
+
+def parse_node(data, idx):
+    nr_children = data[idx]
+    idx += 1
+    nr_meta = data[idx]
+    idx += 1
+    node = Node()
+    for i in range(nr_children):
+        idx, child = parse_node(data, idx)
+        node.children.append(child)
+    for i in range(nr_meta):
+        node.meta.append(data[idx])
+        idx += 1
+    return idx, node
+
+def calc_value(node):
+    value = 0
+    nr_children = len(node.children)
+    if nr_children == 0:
+        for n in node.meta:
+            value += n
+    else:
+        for n in node.meta:
+            n -= 1  # convert into list index
+            if (n >= 0) and (n < nr_children):
+                value += calc_value(node.children[n])
+    return value
+
+def process(data):
+    idx, root = parse_node(data,0)
+    value = calc_value(root)
+    print("Value = " + str(value))
+
+def day8(fname):
+    data = read_data(fname)
+    process(data)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: day8 input.txt") 
+    else:
+    	day8(sys.argv[1])
